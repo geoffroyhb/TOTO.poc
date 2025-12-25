8ae7b30c76ecbc8dca739d1b93accff825c7001e
geoffroyhb <79906373+geoffroyhb@users.noreply.github.com>
2025-12-25 23:57:41 +0100

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.urls import path
+from .views import test_upload
 
 def home(request):
     return HttpResponse("TOTO.poc est en ligne 🚀")
 
 urlpatterns = [
     path("", home),
+    path("test", test_upload),
 ]
