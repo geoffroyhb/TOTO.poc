084ab7edba4e626fad1df1414c5c624977378a41
geoffroyhb <79906373+geoffroyhb@users.noreply.github.com>
2025-12-26 00:15:31 +0100

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,12 +1,16 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 @csrf_exempt
 def test_upload(request):
     if request.method == "GET":
         return render(request, "test_upload.html")
 
-    return HttpResponse("POST reçu ✅")
+    f = request.FILES.get("file")
+    if not f:
+        return HttpResponse("Aucun fichier reçu", status=400)
+
+    return HttpResponse(f"Fichier reçu ✅ : {f.name} ({f.size} bytes)")
 
 
