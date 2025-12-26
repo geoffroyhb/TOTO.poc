2d043b803143855c0bfab7f3c05e3815e1684b17
geoffroyhb <79906373+geoffroyhb@users.noreply.github.com>
2025-12-26 12:31:34 +0100

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,17 +1,26 @@
 from django.http import HttpResponse
 from django.urls import path
-from .views_quiz import join_page, question_page, result_page, seed_questions, create_session
+
+from .views_quiz import (
+    join_page,
+    question_page,
+    result_page,
+    seed_questions,
+    create_session,
+)
 
 def home(request):
     return HttpResponse("TOTO.poc est en ligne 🚀")
 
 urlpatterns = [
     path("", home),
+
+    # élèves
     path("join", join_page),
     path("q/<uuid:attempt_id>/<int:order>", question_page),
     path("result/<uuid:attempt_id>", result_page),
 
-    # admin / seed
+    # admin
     path("seed", seed_questions),
     path("create-session", create_session),
 ]
